9e9bbf7ce9d96cbba50926e1b8e38ad5145c28ae
Aslı Öztürk <ozturkaslii@gmail.com>
2018-12-01 22:49:16 +0300

    django initial commit
    initial commit

---
+++ GenderedTweets/GenderedTweets/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for GenderedTweets project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GenderedTweets.settings')
+
+application = get_wsgi_application()
