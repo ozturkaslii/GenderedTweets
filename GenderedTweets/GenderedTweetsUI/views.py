b0e2ccefd66acab965f9c23ebc0f7cb3f1c61f0c
Aslı Öztürk <ozturkaslii@gmail.com>
2018-12-02 04:07:58 +0300

    UI improvements

--- GenderedTweets/GenderedTweetsUI/views.py
+++ GenderedTweets/GenderedTweetsUI/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'GenderedTweetsUI/index.html', {})
+
+def analysis(request):
+    return render(request, 'GenderedTweetsUI/analysis.html', {})
