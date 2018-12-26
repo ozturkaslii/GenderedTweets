123e34a0eb060af362e52c1d4b4a4354d6c5ae1e
ozturkaslii <ozturkaslii@gmail.com>
2018-12-27 00:15:50 +0300

    wsgi updated for heroku

--- GenderedTweets/GenderedTweets/settings.py
+++ GenderedTweets/GenderedTweets/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
