cccde85693bc817ff50ba437543d0b8e604b6064
ozturkaslii <ozturkaslii@gmail.com>
2018-12-27 07:18:25 +0300

    test

--- GenderedTweets/GenderedTweetsUI/tests.py
+++ GenderedTweets/GenderedTweetsUI/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Tweet
 
 # Create your tests here.
+
+class TwitterTest(TestCase):
+    def test_get_from_db(self):
+        tweet = Tweet.objects.all()
+        self.assertGreater(len(tweet), 0, "Cant't get tweets from database.")
